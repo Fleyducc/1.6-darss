e9fa1a05221c27a6179bdc2d6c240ed077ee8425
Fleyducc <167763678+Fleyducc@users.noreply.github.com>
2024-09-03 15:13:22 +0500

    Add files via upload

---
+++ 1.6 dars/main.py
@@ -0,0 +1,50 @@
+username = "superuser"
+password = "salom123"
+
+input_user = input("Username kiriting: ")
+input_pass = input("Password kiriting: ")
+
+if input_user == username and input_pass == password:
+    print("Xush kelibsiz")
+else:
+    print("Xayr")
+
+#3
+
+son = int(input("Son kiriting: "))
+
+if son % 2 == 0:
+    print("Juft son")
+else:
+    print("Toq son")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# isRich = True
+# if isRich: 
+#     print("I can buy everything I want!")
+# else:
+#     print("I am very poor. I can't buy nothing!")
+
+#     # 1 zadaniya shartmas
+
+    
+ 
